22759bb28b9354dbb63552c7e55c80c62c195f31
Ian Brown <bigianb@gmail.com>
2015-11-04 22:13:48 +0000

    update to params

--- Source/BgdaDrawSpriteBlock.cpp
+++ Source/BgdaDrawSpriteBlock.cpp
@@ -16,13 +16,19 @@ unsigned int BgdaDrawSpriteBlock::Execute()
 	CGSH_HleOgl* gs = (CGSH_HleOgl*)m_vm.GetGSHandler();
 
 	uint8* pTexData = HleVMUtils::getOffsetPointer(m_context, CMIPS::A0, 0);
-	uint32 ypos = m_context.m_State.nGPR[CMIPS::A1].nV0;
-	uint32 xpos = m_context.m_State.nGPR[CMIPS::A2].nV0;
+	uint32 xpos = m_context.m_State.nGPR[CMIPS::A1].nV0;
+	uint32 ypos = m_context.m_State.nGPR[CMIPS::A2].nV0;
 	
 	uint32 arg3 = m_context.m_State.nGPR[CMIPS::A3].nV0;
 	uint32 arg4 = m_context.m_State.nGPR[CMIPS::T0].nV0;
 
 	uint32 vertexRGBA = m_context.m_State.nGPR[CMIPS::T1].nV0;
+
+	int16* pTexData16 = (int16*)pTexData;
+	uint16 texWidth = pTexData16[0];
+	uint16 texHeight = pTexData16[1];
+
+	// GS packet data starts at pTexData+0x80
 
 	m_context.m_State.nPC = m_context.m_State.nGPR[CMIPS::RA].nV0;
 	return 1000;
