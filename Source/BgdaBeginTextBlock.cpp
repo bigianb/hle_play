9bb79aa2f7489fe296ad0f0f3ae3ace6ec3165e6
Ian Brown <bigianb@gmail.com>
2016-04-25 08:18:54 +0100

    font debugging

--- Source/BgdaBeginTextBlock.cpp
+++ Source/BgdaBeginTextBlock.cpp
@@ -1,5 +1,6 @@
 #include "BgdaBeginTextBlock.h"
 #include "bgdaContext.h"
+#include "HleVMUtils.h"
 
 #include "Log.h"
 #define LOG_NAME	("bgda")
@@ -16,12 +17,15 @@ BgdaBeginTextBlock::~BgdaBeginTextBlock()
 unsigned int BgdaBeginTextBlock::Execute()
 {
 	bgdaContext.currentTextColour = m_context.m_State.nGPR[CMIPS::A3].nV0;
+	uint32 fontAddr = m_context.m_State.nGPR[CMIPS::A0].nV0;
 
 	CLog::GetInstance().Print(LOG_NAME, "BgdaBeginTextBlock\n");
 
+	uint32 preStoredFontAddr = HleVMUtils::readInt32Indirect(m_context, CMIPS::GP, 0xbfc4);
+	int cycles = CBasicBlock::Execute();
 
-	return CBasicBlock::Execute();
+	uint32 storedFontAddr = HleVMUtils::readInt32Indirect(m_context, CMIPS::GP, 0xbfc4);
 
 	//m_context.m_State.nPC = m_context.m_State.nGPR[CMIPS::RA].nV0;
-	//return 10;
+	return cycles;
 }
